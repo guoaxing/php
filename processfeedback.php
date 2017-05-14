70c0545e25fdeb8af87ab2d8a4b75b8cf2c6bbcc
guoaxing <18052135@qq.com>
2017-05-14 11:34:03 +0800

    test string

---
+++ processfeedback.php
@@ -0,0 +1,66 @@
+<?php 
+	$name=$_POST['name'];
+	$email=$_POST['email'];
+	$feedback=$_POST['feedback'];
+	$toaddress="18052135@qq.com";
+	$subject="Feedback from web site";
+	$mailcontent="Customer name:".$name."\n".
+				 "Customer email:".$email."\n".
+				 "Customer comments:".$feedback."\n";
+	$fromaddress="From:18052135@qq.com";
+	/*mail($toaddress, $subject, $mailcontent,$fromaddress);*/
+	
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bob's Auto Parts-Feedback Submitted</title>
+</head>
+<body>
+<h1>Feedback submitted</h1>
+<p>Your feedback has been sent</p>
+<?php
+echo "Mailcontent is :$mailcontent"."<br/>";
+echo "with nl2br"."<br/>";
+// test nl2br()
+echo nl2br($mailcontent)."<br/>";
+// test ucwords()
+ echo ucwords(nl2br($mailcontent))."<br/>";
+// test addslashes()
+ echo addslashes($mailcontent)."<br/>";
+//test stirpslashes
+ echo stripslashes(addslashes($mailcontent))."<br/>";
+ //test explode()
+ $email_array=explode('@',$email);
+ foreach ($email_array as $value) {
+ 	echo "$value"."<br/>";
+ }
+ //test join()/implode()
+ $nums=array(1,2,3);
+ $nums_array=implode("-",$nums);
+ echo "$nums_array"."<br/>";
+//test strtok()
+ $token=strtok($feedback, '"');
+ echo "$token"."<br/>";
+ while ($token!="") {
+ 	$token=strtok(" ");
+ 	echo "$token"."<br/>";
+ }
+//test substr()
+ $test='Your name is guo';
+ echo substr($test, 2)."<br/>";
+ echo substr($test, -3)."<br/>";
+ echo substr($test, 5,4)."<br/>";
+  echo substr($test, 5,-4)."<br/>";
+//test strcmp()
+  $str1="2";
+  $str2="12";
+  echo strcmp($str1, $str2)."<br/>";
+  echo strcasecmp($str1, $str2)."<br/>";
+  echo strnatcmp($str1, $str2)."<br/>";
+?>
+
+</body>
+</html>
