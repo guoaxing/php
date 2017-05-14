c7eb8c9bce592d346a26e9550c070fe7f577283a
guoaxing <18052135@qq.com>
2017-05-14 21:24:00 +0800

    test require()

---
+++ footer.php
@@ -0,0 +1,11 @@
+<table width="100%" bgcolor="black" cellpadding="12" border="0">
+	<tr>
+		<td>
+			<p class="foot">&copy;TLA Consulting Ltd.</p>
+			<p class="foot">Please see our
+			<a href="legal.php">legal information</a></p>
+		</td>
+	</tr>
+</table>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,11 @@
+<?php 
+	require('header.php');
+?>
+<p>Welcome to the home of TLA Consulting.
+Please take some time to get to konw us.</p>
+<p>We specialize in serving your business needs
+and hope to hear from you soon.</p>
+<?php
+	require('footer.php');
+?>
+?>
