f74cee99412ddc0a20e8f80977b0d3dabc07004b
guoaxing <18052135@qq.com>
2017-05-22 23:49:26 +0800

    use MySQL

---
+++ MySQL/chapter10-useMySQL/update.sql
@@ -0,0 +1,17 @@
+use books1;
+
+-- update customers
+-- set address='Changan St.'
+-- where customerid=3;
+
+-- alter table customers
+-- modify name char(70) not null;
+
+-- alter table orders
+-- add taxxx float(6,2) after amount;
+
+-- alter table orders
+-- drop taxxx;
+
+delete from customers
+where customerid=5;
