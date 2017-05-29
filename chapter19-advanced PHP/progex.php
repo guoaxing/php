3d579b2eff86dfd8fdb38f44ea8cfd56ed90704f
guoaxing <18052135@qq.com>
2017-05-29 10:13:44 +0800

    test exec

---
+++ chapter19-advanced PHP/progex.php
@@ -0,0 +1,29 @@
+<?php
+	/*chdir('E:/wamp64/www/uploads/');
+	echo '<pre>';
+	exec('dir',$result);
+	foreach ($result as $line) {
+		# code...
+		echo "$line\n";
+	}
+	echo "</pre>";
+	echo "<br><br>";
+
+	echo "<pre>";
+	passthru('dir');
+	echo '</pre>';
+	echo "<br><br>";
+
+	echo "<pre>";
+	$result=system('dir');
+	echo '</pre>';
+	echo "<br><br>";
+
+	echo "<pre>";
+	$result= `dir`;
+	echo $result;
+	echo '</pre>';
+	echo "<br><br>";*/
+	echo 'getenv("HTTP_REFERER")';
+
+?>
